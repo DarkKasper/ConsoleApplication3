328ad8b0767a54d213d0bb54ef063f5657349877
тум тум <asya_makhno@mail.ru>
2023-11-23 16:02:16 +0300

    2

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,12 +1,14 @@
 // ConsoleApplication3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 
 int main()
 {
+    int a;
+    std::cin >> a;
     std::cout << "Hello World!\n";
-    //std::cout << "Hello World!\n";
+    std::cout << std::endl;
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
